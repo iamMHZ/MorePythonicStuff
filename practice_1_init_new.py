7b6161f6623d9742a84bc72105b23b5a868beb5c
iamMHZ <iammhz77@gmail.com>
2021-02-26 15:16:25 +0330

    __init__ vs __new__

---
+++ practice_1_init_new.py
@@ -0,0 +1,71 @@
+# showing the order of them 
+
+class Test1:
+
+    def __new__(cls, *args, **kwargs):
+        print('__new__ is called')
+        return super().__new__(cls)
+
+    def __init__(self):
+        print('__init__ is called')
+
+
+# __new__ creates an object and __int__ initializes that object
+class Test2:
+
+    def __new__(cls, *args, **kwargs):
+        return super().__new__(cls)
+
+    def __init__(self):
+        self.name = 'hey this is my name'
+
+    def __str__(self):
+        return self.name
+
+
+# What if we don't return anything in the __new__
+class Test3:
+    # __new__ doest not return an object
+    def __new__(cls, *args, **kwargs):
+        print('hey __new__ does not return an object')
+
+    def __init__(self):
+        self.name = 'hey this is my name'
+
+    def __str__(self):
+        return self.name
+
+
+class Test4:
+    # while __new__ doest not return an object the __init is not gonna be called
+    def __new__(cls, *args, **kwargs):
+        print('hey __new__ does not return an object So __init__ is not called')
+
+    def __init__(self):
+        print('Hey we do not call __init__ when __new__ does not return an object ')
+
+
+# Can we return a different class in the __new__
+
+class Test5:
+
+    def __new__(cls, *args, **kwargs):
+        return list()
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    # test = Test1()
+
+    # test = Test2()
+    # print(test)
+
+    # test = Test3()
+    # print(test)
+
+    # test = Test4()
+
+    test = Test5()
+    print(type(test))
