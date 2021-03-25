e26e0e42bf80ebaa1bcf86326ca6d1cdcb2aff0a
iamMHZ <iammhz77@gmail.com>
2021-03-26 00:46:23 +0430

    a simple test cases in python

--- examples/code5.py
+++ examples/code5.py
@@ -1,1 +1,19 @@
 # UnitTesting
+
+import unittest
+from utils import multiply
+
+
+# Example 1
+# A simple test case in python unittest module
+class SimpleTestCase(unittest.TestCase):
+
+    def test_negative_numbers(self):
+        expected = 3
+        actual = multiply(-1, -3)
+
+        self.assertEqual(expected, actual, msg='Result of multiplicity')
+
+
+if __name__ == '__main__':
+    unittest.main()
