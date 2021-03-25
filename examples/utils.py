54c80a688510217c6c28902c472482e89e354a26
iamMHZ <iammhz77@gmail.com>
2021-03-26 00:30:39 +0430

    utils module

---
+++ examples/utils.py
@@ -0,0 +1,8 @@
+# Here I create some classes and functions that are used in other modules mainly in unit testing module
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
