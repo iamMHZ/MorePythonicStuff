3b2260f184df6d5c799f2f8b58831194502c14f1
Mohammad Hossein Ziyaaddini <iammhz77@gmail.com>
2021-03-30 03:25:24 +0430

    teardown and setup in the unittests

--- examples/code5.py
+++ examples/code5.py
@@ -15,5 +15,37 @@ class SimpleTestCase(unittest.TestCase):
         self.assertEqual(expected, actual, msg='Result of multiplicity')
 
 
+# Example 2
+# setUp and tearDown methods
+class SimpleTestCase2(unittest.TestCase):
+    def setUp(self):
+        """
+        setUp method runs before each test method
+        """
+        print('Setting up a test')
+
+    def setUpClass(cls):
+        """
+        Runs once before all the tests
+        """
+        pass
+
+    def test_multiply_by_one(self):
+        expected = 1
+        actual = multiply(1, 1)
+
+        self.assertEqual(expected, actual)
+
+    def tearDown(self):
+        """
+        tearDown method runs after each test method
+        """
+        print('Tearing down')
+
+    def tearDownClass(cls):
+        """Runs once at the ends of the tests"""
+        pass
+
+
 if __name__ == '__main__':
     unittest.main()
